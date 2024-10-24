f311ba7cce06c2c3e3bb4647b2d921a82068aa2a
kyoredes <kyoredes@gmail.com>
2024-10-24 22:01:36 +1000

    root & users apps add

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+# from rest_framework import routers
+# from readlist.users import views
+# from django.urls import path
+
+# router = routers.DefaultRouter()
+# router.register('users', views.UserViewSet)
+
+# urlpattterns = [
+#     path()
+# ]
