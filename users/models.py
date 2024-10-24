82ceeabb019913f1f04c5f8211dbf9116fd439b9
kyoredes <kyoredes@gmail.com>
2024-10-25 00:32:23 +1000

    users model update

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from readlist.models import Readlist
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    list = models.ManyToManyField(
+        Readlist,
+        related_name='lists',
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
