cfa2098c085c58911b46bf3300e457b4d03f98c2
kyoredes <kyoredes@gmail.com>
2024-11-11 00:53:55 +1000

    users model change

--- users/models.py
+++ users/models.py
@@ -7,7 +7,8 @@ class CustomUser(AbstractUser):
     booklist = models.ManyToManyField(
         Booklist,
         related_name='lists',
+        blank=True,
     )
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name} | {self.username}"
