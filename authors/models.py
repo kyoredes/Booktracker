589da3c875be5f659bbe875f1ca828e22c3e0d3f
kyoredes <kyoredes@gmail.com>
2024-11-13 22:35:09 +1000

    book model change

--- authors/models.py
+++ authors/models.py
@@ -6,10 +6,8 @@ class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     pen_name = models.CharField(max_length=40)
-    book = models.ForeignKey(
+    book = models.ManyToManyField(
         Book,
         related_name='authors',
-        on_delete=models.CASCADE,
         blank=True,
-        null=True,
     )
