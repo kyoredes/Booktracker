b66aa3b13e948e71d7e5e82b19e8f06a03a5db47
kyoredes <kyoredes@gmail.com>
2024-11-13 22:40:37 +1000

    author urls fix - basename remove

--- authors/urls.py
+++ authors/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from authors.views import AuthorAPIViewSet
 
 router = routers.DefaultRouter()
-router.register('', AuthorAPIViewSet, basename='authors')
+router.register('', AuthorAPIViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
