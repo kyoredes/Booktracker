ae1adca15c7f8ac61f10504d67bdc645fb60d226
kyoredes <kyoredes@gmail.com>
2024-11-13 22:42:56 +1000

    permission IsAdminOrReadOnly fix

--- readlist/permissions.py
+++ readlist/permissions.py
@@ -9,7 +9,7 @@ class IsOwnerOrReadOnly(BasePermission):
 
 
 class IsAdminOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
         return request.user.is_authenticated and request.user.is_staff
