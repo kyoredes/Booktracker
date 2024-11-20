8f9c90947b60e901ec26fbe1a21e8b7f58bee3ef
kyoredes <kyoredes@gmail.com>
2024-11-20 22:14:03 +1000

    author model book field remove

--- authors/models.py
+++ authors/models.py
@@ -1,13 +1,7 @@
 from django.db import models
-from books.models import Book
 
 
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     pen_name = models.CharField(max_length=40)
-    book = models.ManyToManyField(
-        Book,
-        related_name='authors',
-        blank=True,
-    )
