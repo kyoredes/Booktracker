7a0f7003ca9fb781e686fb7208f5c2295a3d1d00
kyoredes <kyoredes@gmail.com>
2024-11-20 22:20:22 +1000

    booklists: delete HiddenField from serializers

--- booklists/serializers.py
+++ booklists/serializers.py
@@ -4,7 +4,6 @@ from users.serializers import UserSerializer
 
 
 class BooklistSerializer(serializers.HyperlinkedModelSerializer):
-    creator = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
         model = Booklist
