bd7dfe64a7be7f22c53b04b85f2c317a5f2d6608
kyoredes <kyoredes@gmail.com>
2024-11-21 16:32:02 +1000

    permissionns fix | add

--- booklists/views.py
+++ booklists/views.py
@@ -2,9 +2,10 @@ from rest_framework import viewsets
 from booklists.models import Booklist
 from booklists.serializers import BooklistSerializer
 from booktracker.permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 
 class BooklistAPIViewSet(viewsets.ModelViewSet):
     queryset = Booklist.objects.all()
     serializer_class = BooklistSerializer
-    permission_classes = [IsOwnerOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly, IsAuthenticated]
