d5d1285e7c4786fcbded5916c47fd35d9272a23e
kyoredes <kyoredes@gmail.com>
2024-11-21 16:32:37 +1000

    authors: serializer fix

--- authors/serializers.py
+++ authors/serializers.py
@@ -1,13 +1,8 @@
 from authors.models import Author
-from books.models import Book
 from rest_framework import serializers
 
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
-    book = serializers.PrimaryKeyRelatedField(
-        queryset=Book.objects.all(),
-        many=True,
-    )
 
     class Meta:
         model = Author
